3b705cc69efc8488edf89a32b29a1e2df183670d
guigran <70112245+guigran@users.noreply.github.com>
2020-10-07 17:53:23 -0300

    HOTFIX

--- T2S COINTAINER-MOVIMENTAÇÃO/src/java/controllers/ContainerControle.java
+++ T2S COINTAINER-MOVIMENTAÇÃO/src/java/controllers/ContainerControle.java
@@ -5,58 +5,91 @@ import model.Movimentacao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-
+import java.util.ArrayList;
+import java.sql.ResultSet;
+        
+        
 public class ContainerControle {
 
-  private Connection connection;
-  private PreparedStatement stmt;
+    private Connection connection;
+    private PreparedStatement stmt;
+    private Statement st;
+    private ArrayList<Container> lista = new ArrayList<>();
+    private ResultSet rs;
 
-  public ContainerControle() {
-    connection = new ConnectionFactory().getConnection();
-  }
+    public ContainerControle() {
+        connection = new ConnectionFactory().getConnection();
+    }
 
-  public void inserir(Container container) {
-    String sql = "insert into container (nm_cliente, id_container, nm_tipo, ic_status_cheio_vazio, nm_categoria_import_export) VALUES ( ?, ?, ?, ?, ?)";
+    public void inserir(Container container) {
+        String sql = "insert into container (nm_cliente, id_container, nm_tipo, ic_status_cheio_vazio, nm_categoria_import_export) VALUES ( ?, ?, ?, ?, ?)";
+
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, container.getNm_cliente());
+            stmt.setString(2, container.getId_container());
+            stmt.setString(3, container.getNm_tipo());
+            stmt.setString(4, container.getIc_status_cheio_vazio());
+            stmt.setString(5, container.getNm_categoria_import_export());
+            stmt.execute();
+            stmt.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inserir" + e);
+        }
+    }
+
+    public void alterar(Container container) {
+        String sql = "UPDATE container SET  ";
+
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, container.getNm_cliente());
+            stmt.setString(2, container.getId_container());
+            stmt.setString(3, container.getNm_tipo());
+            stmt.setString(4, container.getIc_status_cheio_vazio());
+            stmt.setString(5, container.getNm_categoria_import_export());
+            stmt.setInt(6, container.getCd_container());
+            stmt.execute();
+            stmt.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao alterar" + e);
+        }
+    }
+
+    public void deletar(int valor) {
+        String sql = "DELETE FROM container WHERE cd_container= " + valor;
+        try {
+            st = connection.createStatement();
+            st.execute(sql);
+            st.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao inserir" + e);
+        }
+
+    }
+
+
+public ArrayList<Container> listar() {
+    String sql = "SELECT * FROM container";
 
     try {
-      stmt = connection.prepareStatement(sql);
-      stmt.setString(1, container.getNm_cliente());
-      stmt.setString(2, container.getId_container());
-      stmt.setString(3, container.getNm_tipo());
-      stmt.setString(4, container.getIc_status_cheio_vazio());
-      stmt.setString(5, container.getNm_categoria_import_export());
-      stmt.execute();
-      stmt.close();
-
+      st = connection.createStatement();
+      rs = st.executeQuery(sql);
+      while (rs.next()) {
+        Container container = new Container();
+        container.setCd_container(rs.getInt("cd_container"));
+        container.setNm_cliente(rs.getString("nm_cliente"));
+        container.setId_container(rs.getString("id_container"));
+        container.setNm_tipo(rs.getString("nm_tipo"));
+        container.setIc_status_cheio_vazio(rs.getString("ic_status_cheio_vazio"));
+        container.setNm_categoria_import_export(rs.getString("nm_categoria_import_export"));    
+        lista.add(container);
+      }
     } catch (Exception e) {
-      throw new RuntimeException("Erro ao inserir" + e);
+      throw new RuntimeException("Erro ao listar" + e);
     }
+    return lista;
   }
 
-  public void alterar(Container container) {
-    String sql = "UPDATE container SET  ";
-
-    try {
-      stmt = connection.prepareStatement(sql);
-      stmt.setString(1, container.getNm_cliente());
-      stmt.setString(2, container.getId_container());
-      stmt.setString(3, container.getNm_tipo());
-      stmt.setString(4, container.getIc_status_cheio_vazio());
-      stmt.setString(5, container.getNm_categoria_import_export());
-    } catch (Exception e) {
-      throw new RuntimeException("Erro ao alterar" + e);
-    }
-  }
-
-  public void deletar(Container container) {
-    String sql = "";
-
-    try {
-
-    } catch (Exception e) {
-      throw new RuntimeException("Erro ao inserir" + e);
-    }
-  }
 }
-
-
